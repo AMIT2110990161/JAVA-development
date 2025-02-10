1f4dcfd6b87adfc8f29a39e116a707fd4e11c083
AMIT KUMAR <amit70kumar14@gmail.com>
2025-02-10 11:48:58 +0530

    weak02_01

---
+++ bridgeLabz practice problems/practiceProblem_Weak02/Monday_10_feb_2025/Book.java
@@ -0,0 +1,21 @@
+public class Book {
+    String title;
+    String auther;
+    float price;
+
+    Book(String title, String auther, float price){ // constructor -> initilizing the value of instance variable
+        this.title = title;
+        this.auther = auther;
+        this.price = price;
+    }
+
+    public void bookDetails(){
+        System.out.println("Title of book : " + title);
+        System.out.println("Auther of book : " + auther);
+        System.out.println("Price of book : " + price);
+    }
+    public static void main(String[] args) {
+        Book bk = new Book("Raga fit", "Amit kumar", 28.5f);
+        bk.bookDetails();
+    }
+}
