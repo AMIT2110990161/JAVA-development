da0cc1ac43654428fa0135d5d89414148dcb0dfa
AMIT KUMAR <amit70kumar14@gmail.com>
2025-03-10 14:55:30 +0530

    new

---
+++ bridgeLabz practice problems/DSA/src/LinkedList/StudentRecordManagement.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+public class StudentRecordManagement {
+    public static void main(String[] args) {
+        StudentLinkedList sll = new StudentLinkedList();
+    }
+}
+
+class StudentLinkedList{
+    StudentLinkedList(){
+        System.out.println("Namestay java");
+    }
+}
